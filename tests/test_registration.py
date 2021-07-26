3a4ab877dfa36f7ea532ed2b85c4b31bc9e937c5
Dobos Brigi <dobosbrigi91@gmail.com>
2021-07-26 21:17:11 +0200

    test TC01 - Registration

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -7,9 +7,19 @@ import csv
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+
+
 wait = WebDriverWait(driver, 10)
+
 
 
 def find_and_clear(xpath):
