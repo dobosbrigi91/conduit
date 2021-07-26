a0155ec47e8e0ec2e88d579d90f72d4c2bf93e40
Dobos Brigi <dobosbrigi91@gmail.com>
2021-07-26 20:40:36 +0200

    test TC01 - Registration

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,7 +1,5 @@
 # TC01 - Registration
 
-# imports:
-
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -10,13 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--window-size=1420,1080')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-
-driver = webdriver.ChromeChromeDriverManager().install()
+driver = webdriver.Chrome(ChromeDriverManager().install())
 wait = WebDriverWait(driver, 10)
 
 
