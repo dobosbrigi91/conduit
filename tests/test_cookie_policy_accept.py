f2c190245f01016fbe27ec685a08067f8ecb1f9e
Dobos Brigi <dobosbrigi91@gmail.com>
2021-07-26 16:54:37 +0200

    test cookie policy accept

---
+++ tests/test_cookie_policy_accept.py
@@ -0,0 +1,25 @@
+# TC04 - Cookie Policy - accept
+
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+
+
+def test_cookie_policy_accept():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    try:
+        driver.get('http://localhost:1667')
+
+        # learn more:
+        learn_more = driver.find_element_by_xpath('//div[@id="cookie-policy-panel"]//a')
+        learn_more.click()
+        cookie_policy = driver.switch_to.window(driver.window_handles[-1])
+        time.sleep(3)
+        driver.close()
+        driver.switch_to.window((driver.window_handles[0]))
+
+        # Accept:
+        accept_button = driver.find_element_by_xpath('//div//button[contains(@class, "accept")]')
+        accept_button.click()
+    finally:
+        driver.close()
