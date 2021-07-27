21714a2eee38d33de11b1c96118ff230e6556223
Dobos Brigi <dobosbrigi91@gmail.com>
2021-07-27 08:54:42 +0200

    test TC02 - Login

--- tests/test_login.py
+++ tests/test_login.py
@@ -11,6 +11,8 @@ from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--disable-gpu')
+options.add_argument('--disable-extensions')
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 wait = WebDriverWait(driver, 10)
